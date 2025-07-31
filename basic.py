a46af250972c7cd987032694643ac15ecd62624b
Automation Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-08-01 00:54:31 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,11 @@
+def method1():
+  print("Hello World")
+
+
+def test_method2():
+  print("This is PyTest case")
+  assert 5 == 5
+
+# `method1()` ek simple function hai jo "Hello World" print karta hai. 
+# `test_method2()` ek PyTest test case hai jo "This is PyTest case" print karta hai 
+# aur `assert 5 == 5` condition check karta hai. Kyunki condition true hai, test successfully pass ho jaata hai bina kisi error ke.
