683202a43f6ef7189b6701a9e89bfd5ffea75919
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-10-19 19:24:07 +0530

    Update 05 skip.py

--- 05 skip.py
+++ 05 skip.py
@@ -1,3 +1,6 @@
+# pytest main.py --alluredir=reports
+# allure serve reports
+
 import pytest
 import allure
 import requests
@@ -13,7 +16,6 @@ def test_basic_math():
 def test_sub1():
     assert 3 - 3 == 0
 
-
 @allure.title("TC#3 - verify that 1-1 is equal to 0")
 @allure.description("This is a smoke Testcase which check - verify that 1-1 is equal to 0")
 @pytest.mark.smoke
@@ -23,10 +25,3 @@ def test_sub2():
 @pytest.mark.skip(reason="Not working,Skip it")
 def test_sub3():
     assert 0 - 0 != 0
-
-# Yeh code PyTest, Allure aur Requests libraries ka use karta hai basic subtraction test cases ke liye.
-# `test_basic_math()` check karta hai `2-2 == 0`, jise `@tapas` marker diya gaya hai.
-# `test_sub1()` aur `test_sub2()` regression aur smoke test cases hain jo `3-3` aur `1-1` ke liye `0` validate karte hain.
-# Allure ke `@title` aur `@description` se report readable aur structured banti hai.
-# `test_sub3()` ko skip kiya gaya hai with reason `"Not working"`, taaki woh test run na ho.
-# Ye test cases beginners ke liye automation testing samajhne ka accha example hai.
