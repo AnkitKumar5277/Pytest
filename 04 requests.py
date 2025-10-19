cd685ed29d9a9d6338bccc429f3ed68fd4419147
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-10-19 19:09:39 +0530

    Update 04 requests.py

--- 04 requests.py
+++ 04 requests.py
@@ -22,3 +22,18 @@ def test_get_request_negative():
     response_data = requests.get(url=url_get)
     assert response_data.status_code == 404
     print("negative")
+
+# method 2
+@allure.title("Verify the GET Request of Restful Booker")
+@allure.description("This Testcase check Booking 1 and verify the response")
+@pytest.mark.positive
+def test_get_request_positive():
+    assert requests.get(url="https://restful-booker.herokuapp.com/booking/1").status_code == 200
+    print("positive")
+
+@allure.title("Verify the GET Request of Restful Booker with invalid ID")
+@allure.description("This Testcase check Booking -1 and verify the response")
+@pytest.mark.positive
+def test_get_request_negative():
+    assert requests.get(url="https://restful-booker.herokuapp.com/booking/-1").status_code == 404
+    print("negative")
